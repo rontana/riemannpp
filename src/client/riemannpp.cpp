e22ef5d8b85763b87bfec27e7b0ae7448d2777e5
Christopher Gilbert <christopher.john.gilbert@gmail.com>
2014-08-16 18:10:15 +0100

    Latest tweaks to Makefile and added missing client test program

---
+++ src/client/riemannpp.cpp
@@ -0,0 +1,13 @@
+#include <riemannpp/riemannpp.hpp>
+#include <riemann/riemann-client.h>
+#include <iostream>
+
+using namespace std;
+namespace rpp = riemannpp;
+
+int main() {
+	rpp::client client;
+	riemann_client_t *riemann_client_new();
+	cout << "Hello world!" << endl;
+	return EXIT_SUCCESS;
+}
