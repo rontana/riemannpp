74b1b02bc4fd8c9ce84fbe230b1eb953cc415eab
Christopher Gilbert <christopher.john.gilbert@gmail.com>
2014-08-18 21:48:23 +0100

    Removed obsolete options from client options

--- src/client/options.hpp
+++ src/client/options.hpp
@@ -7,54 +7,14 @@
 namespace client {
 
 	struct options {
-		// miscellaneous options
 		bool        show_usage;
 		bool        show_help;
 		bool        show_version;
-
-		// positional options
-		std::string command;
-		std::string rhost;
-		int         rport;
-
-		// send command options
-		std::string state;
-		std::string service;
-		std::string host;
-		std::string description;
-		std::string attributes;
-		std::string tags;
-		int64_t     metric_sint64;
-		double      metric_dbl;
-		float       metric_flt;
-		int         ttl;
-		bool        tcp;
-		bool        udp;
-
-		// query command options
-		std::string query;
-		bool        result_json;
 
 		options()
 			: show_usage(false)
 			, show_help(false)
 			, show_version(false)
-			, command("")
-			, rhost("")
-			, rport(0)
-			, state("")
-			, service("")
-			, host("")
-			, description("")
-			, attributes("")
-			, tags("")
-			, metric_sint64(0)
-			, metric_dbl(0.0)
-			, metric_flt(0.f)
-			, ttl(0)
-			, result_json(false)
-			, tcp(false)
-			, udp(false)
 		{}
 	};
 
