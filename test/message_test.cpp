c11510fa45d8c849b27825ba1af10d15b85ce8bf
Christopher Gilbert <chris.gilbert@datasift.com>
2014-08-20 17:02:45 +0100

    Added more checks to message test

--- test/message_test.cpp
+++ test/message_test.cpp
@@ -10,24 +10,40 @@ TEST_CASE("messages can be created and set", "[message]") {
 		REQUIRE((riemann_message_t*)msg != nullptr);
 	}
 	SECTION("move constructor") {
-
+		riemann::message msg;
+		riemann::message msg2(std::move(msg));
+		REQUIRE((riemann_message_t*)msg == nullptr);
+		REQUIRE((riemann_message_t*)msg2 != nullptr);
 	}
 	SECTION("construct with query") {
-
+		riemann::query q("*");
+		riemann::message msg(q);
+		REQUIRE((riemann_message_t*)msg != nullptr);
 	}
 	SECTION("move assignment operator") {
-
+		riemann::message msg;
+		riemann::message msg2 = std::move(msg);
+		REQUIRE((riemann_message_t*)msg == nullptr);
+		REQUIRE((riemann_message_t*)msg2 != nullptr);
 	}
 	SECTION("set event") {
-
+//		riemann::event e();
+//		riemann::message msg;
+//		msg.set_event(e);
 	}
 	SECTION("stream operator set event") {
-
+//		riemann::event e();
+//		riemann::message msg;
+//		msg << e;
 	}
 	SECTION("set query") {
-
+		riemann::query q("*");
+		riemann::message msg;
+		msg.set_query(q);
 	}
 	SECTION("stream operator set query") {
-
+		riemann::query q("*");
+		riemann::message msg;
+		msg << q;
 	}
 }
